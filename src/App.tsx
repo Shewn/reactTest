c3eb5ae2e46489a6e89fd9f41bff8f5393d6606b
chong_shewn_lee <chong.shewn.lee@accenture.com>
2024-03-20 21:47:42 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css';
 import Portal from './component/login/portal'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const element = <div className="App">
+<div className="loginComponent">
+  <Portal />
+</div>
+</div>;
 function App() {
   return (
-    <div className="App">
-      <div className="loginComponent">
-        <Portal />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={element}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
