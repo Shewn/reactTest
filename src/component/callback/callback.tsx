2a93484e268561f8a40a5ba7bf7303604b6178a2
chong_shewn_lee <chong.shewn.lee@accenture.com>
2024-03-26 15:08:40 +0800

    post to token

--- src/component/callback/callback.tsx
+++ src/component/callback/callback.tsx
@@ -1,12 +1,91 @@
-// PaymentForm.tsx
-
+import * as jose from 'jose'
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 function CallbackPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+
+  useEffect(() => {
+    async function generatePubKey() {
+      const code = searchParams.get("code");
+      console.log(code);
+      const alg = 'ES256'
+      // const additionalOption = {
+      //   "use": "sig",
+      //   "kid": "sig-2021-01-15T12:09:06Z"
+      // }
+      const privateJwk = { crv: 'P-256', d: 'u-tH-27uBwv0Bry-MMQZO0xNq-3mmMpEMzI9dsWrFwk', kty: 'EC', x: 'lDLBk4ohdbJZ7oMaknrKutgnlAzx3bjOa_GDIBM_miA', y: 'U7_iImOHaFig_lYwcWVBDpOszv05Bfn7YVYRBkwnS8Y', enc: "A256CBC-HS512", use: 'sig', kid: "sig-2021-01-15T12:09:06Z" };
+      const publicJwk = { crv: 'P-256', kty: 'EC', x: 'lDLBk4ohdbJZ7oMaknrKutgnlAzx3bjOa_GDIBM_miA', y: 'U7_iImOHaFig_lYwcWVBDpOszv05Bfn7YVYRBkwnS8Y', use: 'sig', kid: "sig-2021-01-15T12:09:06Z" };
+      const dppJwksFormat = [publicJwk];
+      const privateKeyToSign = await jose.importJWK(privateJwk, alg)
+      const publicKeyToVerify = await jose.importJWK(publicJwk, alg)
+      console.log(privateJwk)
+
+      console.log(JSON.stringify({ keys: dppJwksFormat }))
+
+      const jwt = await new jose.SignJWT({})
+        .setProtectedHeader({ alg: alg, kid: "sig-2021-01-15T12:09:06Z", type: "JWT" })
+        .setIssuedAt(Math.floor(Date.now() / 1000))
+        .setSubject('GHJxWcqnnxqCHsmVFeTkY1W6I95wtYze')
+        .setIssuer('GHJxWcqnnxqCHsmVFeTkY1W6I95wtYze')
+        .setAudience('https://stg-id.singpass.gov.sg')
+        .setExpirationTime(Math.floor(Date.now() / 1000) + 100)
+        .sign(privateKeyToSign)
+
+      console.log(jwt);
+    
+      const { payload, protectedHeader } = await jose.jwtVerify(jwt, publicKeyToVerify, {})
+      console.log(payload);
+      console.log(protectedHeader);
+      if (code) {
+        const param = {
+          client_id: 'GHJxWcqnnxqCHsmVFeTkY1W6I95wtYze',
+          grant_type: 'authorization_code',
+          redirect_uri: 'https://heehehooo.netlify.app/callback',
+          code: code,
+          scope: 'openid',
+          client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+          client_assertion: 'jwt'
+        };
+        const host = 'https://stg-id.singpass.gov.sg/token';
+
+        await fetch(host, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: JSON.stringify(param)
+        })
+          .then(response => response.json())
+          .then(response => console.log(JSON.stringify(response)))
+      }
+
+    }
+    generatePubKey();
+  }, [])
 
   return (
-   <div>
-    Callback
-   </div>
+    <div>
+      Callback
+    </div>
   );
 }
 
 export default CallbackPage;
+
+
+// const pub = {keys: [
+//   { crv: 'P-256', 
+//   kty: 'EC', 
+//   x: 'lDLBk4ohdbJZ7oMaknrKutgnlAzx3bjOa_GDIBM_miA', 
+//   y: 'U7_iImOHaFig_lYwcWVBDpOszv05Bfn7YVYRBkwnS8Y', 
+//   use: 'sig', kid: "sig-2021-01-15T12:09:06Z",
+//   alg: 'ES256'}, 
+//   { crv: 'P-256', 
+//   kty: 'EC', 
+//   x: 'lDLBk4ohdbJZ7oMaknrKutgnlAzx3bjOa_GDIBM_miA', 
+//   y: 'U7_iImOHaFig_lYwcWVBDpOszv05Bfn7YVYRBkwnS8Y', 
+//   use: 'enc', 
+//   alg: 'ECDH-ES+A128KW',
+//   kid: "sig-2021-01-15T12:09:07Z" }]}
+//   console.log(JSON.stringify(pub));
